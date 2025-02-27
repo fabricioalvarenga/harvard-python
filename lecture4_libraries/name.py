4d8425fcd2ffafbcbfcb52c1e319d1d8ca529821
fabricioalvarenga <fab.oliveira.alvarenga@gmail.com>
2025-02-26 21:52:12 -0300

    Lecture 4 - Libraries

--- lecture4_libraries/name.py
+++ lecture4_libraries/name.py
@@ -1,6 +1,14 @@
 import sys
 
-try:
-    print("Hello, my name is", sys.argv[1])
-except IndexError:
-    print("Too few arguments")
+#try:
+#    print("Hello, my name is", sys.argv[1])
+#except IndexError:
+#    print("Too few arguments")
+
+if len(sys.argv) < 2:
+    sys.exit("Too few arguments")
+
+print("My name is", end = " ")
+for arg in sys.argv[1:]:
+    print(arg, end = " ")
+print()
