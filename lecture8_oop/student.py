47221d2f998c110c5f6be641f7b453d46eb959d7
fabricioalvarenga <fab.oliveira.alvarenga@gmail.com>
2025-03-25 21:42:23 -0300

    Lecture 8 - OOP

--- lecture8_oop/student.py
+++ lecture8_oop/student.py
@@ -1,33 +1,62 @@
 class Student:
-    name: str
-    house: str
-
-    def __init__(self, name:str , house: str):
+    def __init__(self, name, house, patronus):
         self.name = name
         self.house = house
+        self.patronus = patronus
 
-    def get_name(self) -> str:
-        return self.name
+    def __str__(self):
+        return f"{self.name} from {self.house}, patronus {self.charm()}"
 
-    def set_name(self, name: str):
-        self.name = name
+    @property
+    def name(self):
+        return self._name
 
-    def get_house(self) -> str:
-        return self.house
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Missing name")
+        self._name = name
 
-    def set_house(self, house: str):
-        self.house = house
+    @property
+    def house(self):
+        return self._house
+
+    @house.setter
+    def house(self, house):
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self._house = house
+
+    @property
+    def patronus(self):
+        return self._patronus
+
+    @patronus.setter
+    def patronus(self, patronus):
+        self._patronus = patronus
+
+    def charm(self):
+        match self.patronus:
+            case "Stag":
+                return "🐴"
+            case "Otter":
+                return "😎"
+            case "Jack":
+                return "🐶"
+            case _:
+                return "🪄"
+
+    @classmethod
+    def get(cls):
+        name = input("Name:  ")
+        house = input("House: ")
+        patronus = input("Patronus: ")
+        return cls(name, house, patronus)
 
 
 def main():
-    student: Student = get_student()
-    print(f"{student.name} from {student.house}")
-
-
-def get_student() -> Student:
-    name: str = input("Name:  ")
-    house: str = input("House: ")
-    return Student(name, house)
+    student = Student.get()
+    print(student)
 
 
 if __name__ == "__main__":
